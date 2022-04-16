ba9c634752b915d16b361a81e85b5be5064ecbf4
meldasav <gmeldag83@hotmail.com>
2022-04-16 12:01:21 -0500

    added to encapsulation

---
+++ src/encapsulation/Company.java
@@ -0,0 +1,40 @@
+package encapsulation;
+
+public class Company {
+
+    private String name;
+    private String address;
+    private String phoneNumber;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String apple) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override//it is optinal
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+}
