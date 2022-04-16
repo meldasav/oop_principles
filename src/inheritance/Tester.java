1df5b223bee08dfe09257beb530356c82c941338
meldasav <gmeldag83@hotmail.com>
2022-04-16 15:40:09 -0500

    added inheritance

--- src/inheritance/Tester.java
+++ src/inheritance/Tester.java
@@ -13,4 +13,23 @@ public class Tester extends Person {
         System.out.println("tester codes");
     }
 
+    @Override
+    public void sleep() {
+        System.out.println("tester sleeps");
+    }
+
+    @Override
+    public void learn() {
+        System.out.println("tester learns");
+    }
+
+    @Override
+    public void walk() {
+        System.out.println("tester walks");
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("tester eats");
+    }
 }
