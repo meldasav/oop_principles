276067d83b0aff85ca67bcb0e97476f6e434d06e
meldasav <gmeldag83@hotmail.com>
2022-04-19 20:56:41 -0500

    added abstract classes intro

---
+++ src/abstraction/abstract_classes/Phone.java
@@ -0,0 +1,37 @@
+package abstraction.abstract_classes;
+
+public abstract class Phone {
+    public Phone(){
+
+    }
+
+    public Phone(String brand, String color, int storage, double price) {
+        this.brand = brand;
+        this.color = color;
+        this.storage = storage;
+        this.price = price;
+    }
+
+    public abstract void call();
+    public abstract void ring();
+    public abstract void text();
+    public abstract boolean isConvertible();
+
+    public String brand;
+    public String color;
+    public int storage;
+    public double price;
+
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "brand='" + brand + '\'' +
+                ", color='" + color + '\'' +
+                ", storage=" + storage +
+                ", price=" + price +
+                '}';
+    }
+}
+
+
---
+++ src/abstraction/abstract_classes/Test.java
@@ -0,0 +1,35 @@
+package abstraction.abstract_classes;
+
+import java.util.ArrayList;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+        Samsung s1 = new Samsung("Samsung", "Black", 64, 700);
+        Nokia n1 = new Nokia("Nokia", "Blue", 4, 100);
+        Iphone i1 = new Iphone("Apple", "Silver", 128, 1000);
+
+
+        ArrayList<Phone> myPhone = new ArrayList<>();
+        myPhone.add(s1);
+        myPhone.add(n1);
+        myPhone.add(i1);
+
+
+        for (Phone phone : myPhone)
+            System.out.println(myPhone);
+
+        int max=Integer.MIN_VALUE;
+        Phone mostExpensivePhone=null;
+
+        int countConvertible = 0;
+        for (Phone phone : myPhone) {
+            if (phone.isConvertible()) countConvertible++;
+            if(phone.price>max)mostExpensivePhone=phone;
+        }
+        System.out.println(countConvertible);
+        System.out.println("the most expensive phone is = " + mostExpensivePhone.getClass().getSimpleName());
+    }
+}
